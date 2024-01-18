90e5d9bd97b0a9db6e54ba327aed6250c118bd04
Eduardo Henrique <edu.hen.fm@gmail.com>
2024-01-18 19:20:02 -0400

    :sparkles: show what input was read

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -14,6 +16,7 @@ void enableRawMode() {
 
   struct termios raw = orig_termios;
   raw.c_lflag &= ~(ECHO);
+  raw.c_lflag &= ~(ECHO | ICANON);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -22,6 +25,13 @@ int main() {
   enableRawMode();
 
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
+
   return 0;
 }
